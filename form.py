a192c5b45cf081d6794ac8745f6a21cded8aba1e
junaid-hub <junaidnazir7501@gmail.com>
2022-09-07 12:19:19 +0500

    Forms is PyQt5

--- form.py
+++ form.py
@@ -8,6 +8,27 @@ class MainWindows(qtw.QWidget):
         # add a title
         self.setWindowTitle('PYQT5 Tutorial')
         
+        # set QForm layout
+        form_layout = qtw.QFormLayout()
+        self.setLayout(form_layout)
+
+        # add widgets
+        label_1 = qtw.QLabel('This is Cool Label Row')
+        label_1.setFont(qtg.QFont('Helvetica',24))
+
+        f_name = qtw.QLineEdit(self)
+        l_name = qtw.QLineEdit(self)
+        
+        # add rows to App
+        form_layout.addRow(label_1)
+        form_layout.addRow('First Name:',f_name)
+        form_layout.addRow('Last Name:',l_name)
+        form_layout.addRow(qtw.QPushButton('Press Me',clicked = lambda: press_it()))
+
+        def press_it():
+            label_1.setText(f'Your Text: {f_name.text()} {l_name.text()}')
+        # show the app
+        self.show()
         
             
 app = qtw.QApplication([])
